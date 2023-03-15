ca6b705266b37c9eb791ee7f0d2333896c4a19ba
Berserk <alexlecerf0131@gmail.com>
2023-03-15 21:03:24 +0000

    started working with axios and added components folder for future coding

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+import axios from 'axios';
+
+
+
 function App() {
+
+  var url = `https://images-api.nasa.gov/search?q=leo`;
+  console.log(url)
+  axios.get(url)
+      .then(function(response) {
+              console.log(response.data.collection.items[0].links[0].href); 
+});
+
   return (
     <div className="App">
       <header className="App-header">
