8d66958a87265fab09d70f6f61e91d9e022e1c15
rizla <ryanpauldutton@gmail.com>
2023-03-16 21:22:57 +0000

    added start of axios GET search

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -5,18 +5,35 @@ import axios from 'axios';
 
 function Home() {
   const [starSearch, setStarSearch ] = useState('');
-console.log(starSearch)
-
-const API_KEY = 'DEMO_KEY';
+const [starImage, setStarImage] = useState({});
+const API_KEY = '6b3Hin4OoLFrPjYUINVedOoT7MlvEm0zfNtMlbu0';
 
 function searchStar(event) {
-console.log('hello')
+  //Set up correct API call
+  // var APICallString = 'https://images-api.nasa.gov/search?q=' + starSearch + '?api_key=' + API_KEY
+  var APICallString = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=' + API_KEY
+  //handle API Call
+axios.get(APICallString).then(function (response){
+//success
+console.log(response)
+setStarImage(response.data);
+
+}).catch(function(err){
+//error
+console.log(err)
+});
 }
 
+const starImageOne = starImage.photos[0].img_src
   return (
     <div className='bckg'>
       <input type='text' onChange={e => setStarSearch(e.target.value)}></input>
       <button onClick={e => searchStar(e)}>Search</button>
+      {/* {//turnerary operator/} */}
+      {JSON.stringify(starImage) != '{}' ? 
+      <><img src={starImageOne} /></>
+    :
+    <><p>We have no Image</p></>}
       <HeroSection />
     </div>
   )
