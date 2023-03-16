2db0a243b4d4bf9068ae34a5850d68d32a7e02da
Berserk <alexlecerf0131@gmail.com>
2023-03-16 21:19:57 +0000

    fixed HomePage page

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -3,10 +3,12 @@ import HeroSection from '../../components/HeroSection';
 import './style.css';
 import axios from 'axios';
 
+
+function Homepage() {
   return (
     <div className='bckg'>
-      <input type='text' onChange={e => setStarSearch(e.target.value)}></input>
-      <button onClick={e => searchStar(e)}>Search</button>
+      {/* <input type='text' onChange={e => setStarSearch(e.target.value)}></input> */}
+      {/* <button onClick={e => searchStar(e)}>Search</button> */}
       <HeroSection />
     </div>
   )
