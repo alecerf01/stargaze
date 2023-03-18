055f97892cc8240ea743f4a2192a8d7b0926fe05
Marzena-W <marzena.m.wilk@gmail.com>
2023-03-18 20:17:18 +0000

    added active page to anvbar with wavy uderline

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -20,7 +20,7 @@ function Navigation() {
                         <ul className="navbar-nav"> 
 
                             <li className="nav-item">
-                                <NavLink to="/" end className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}> Home </NavLink>
+                                <NavLink  aria-current="page" to="/" end className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}> Home </NavLink>
                             </li>
 
                             <li className="nav-item">
