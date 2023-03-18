5f397eca746c75eff0d65f26431055e9b23fd7f8
Marzena-W <marzena.m.wilk@gmail.com>
2023-03-18 11:50:42 +0000

    changes to sentact page, working on favourite page template

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -5,18 +5,23 @@ import Footer from "../../components/Footer";
 function Contact() {
 
     return (
-        <div className='contact-container'>
-            <div className='main-para'>
-                <p className="contact-header">Contact us</p>
-                <p className="para">You can check our GitHub repo below</p>
-            </div>
+        <>
+            <div className='contact-container'>
+                <div className='main-para'>
+                    <p className="contact-header">Contact us</p>
+                    <p className="para">You can check our repo for
+                        <span>
+                            <img src="/logo-black.svg" height="20" className="d-inline-block align-top" alt="stargaze logo" />
+                        </span> below</p>
+                </div>
 
-            <div className='contact-buttons'>
-                <a  target='_blank' href="https://github.com/alecerf01/stargaze"> <button className='buttons' type="button"> GitHub</button></a>
+                <div className='contact-buttons'>
+                    <a target='_blank' href="https://github.com/alecerf01/stargaze"> <button className='buttons' type="button"> GitHub</button></a>
+                </div>
+
             </div>
             <Footer />
-        </div>
-
+        </>
     )
 }
 
--- src/pages/Favourite/index.js
+++ src/pages/Favourite/index.js
@@ -2,19 +2,34 @@ import React from 'react';
 import Card from '../../components/Card';
 import Search from '../../components/Search';
 import Footer from "../../components/Footer";
+import HeroSection from '../../components/HeroSection';
+import Wrapper from "../../components/Wrapper";
 import './style.css';
 
 function Favourite() {
   return (
-    <div className='bckg'>
-        <p className="fave-header">Header</p>
-        <p className="fave-subheader">Neque porro quisquam est qui dolorem ipsum quia dolor adipisci velit.</p>
-        
-        <Search />
-        <Card />
-      
-        <Footer /> 
-    </div>
+    <>
+      <div className='bckg'>
+        <div className='hero-sec'>
+          <HeroSection />
+          <Search />
+        </div>
+
+
+        <div className='constellation-sec'>
+          <p className='constellation-header'>you favourites</p>
+
+          <Wrapper>
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+          </Wrapper>
+        </div>
+
+      </div>
+      <Footer />
+    </>
   )
 }
 
