3b7f425ee3a2f8a92b4cdf0b2f3958cb1a1a02b5
Berserk <alexlecerf0131@gmail.com>
2023-03-19 09:49:42 +0000

    added card fucntionality for seach function

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,19 +1,54 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { FaHeart, FaRegHeart } from "react-icons/fa";
 
-
 {/* <FaHeart className='saved-icon'/> */}
-function Card() {
+
+function Card(props) {
+  function isFavourite(){
+    const savedCards = JSON.parse(localStorage.getItem('cards'))
+
+    if(!savedCards || savedCards.length < 1){
+      return false
+    }
+
+    const isFav = savedCards.find((card) => card.imageLink === props.imageLink)
+
+    return isFav
+  }
+
+  const [isFav, setIsFav] = useState(isFavourite());
+
+ 
+
+  function save() {
+    const savedCards = localStorage.getItem('cards');
+
+    const cardToSave = {
+      imageLink: props.imageLink,
+      cardName: props.cardName, 
+      cardDescription: props.cardDescription
+    }
+
+    // check if card is already Favourite, don't save card again
+
+    // if it's not favourite get card 'JSON.parse' -> array.push -> Json.stringify -> localstorage.setItem('cards', updatedArray)
+
+    // find if the card is already saved
+
+
+  }
+  
   return (
         <div className="card">
           <div className="img-container">
-            <div className='save-icon'><FaRegHeart/></div>
+            <div onClick={save} className='save-icon'><FaRegHeart/></div>
             <img className='card-image'></img>
           </div>
     
           <div className="content">
-            <h2 className='card-name'>Name</h2>
-            <p className='card-para'> Description</p>
+            <img src={props.imageLink} alt="" />
+            <h2 className='card-name'>{props.cardName}</h2>
+            <p className='card-para'>{props.cardDescription}</p>
           </div>
         </div>
       )
